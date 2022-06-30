6bc709e181298bc1ca33108a07056f8534e09ece
Jatin Saini <jatinkhushi5555@gmail.com>
2022-06-30 23:50:01 +0530

    App ready

--- Backend/server.js
+++ Backend/server.js
@@ -16,6 +16,13 @@ require("./DataBase/connection");
 
 const PORT = process.env.PORT || 4000;
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join("../Frontend/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
+    });
+}
+
 app.listen(PORT, () => {
     console.log("Server Started");
 });
